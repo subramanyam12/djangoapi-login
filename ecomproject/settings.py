1e8867724f265ffa0df8f3ae73427364fa377eda
subramanayam12 <subramanyam933@gmail.com>
2023-09-29 20:02:37 +0530

    changing settings

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -92,6 +92,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'check_same_thread': False,  # Add this line to disable determinism
+        },
     }
 }
 
