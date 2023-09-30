a5b6bde97a3a7d4679d9e83d4910828b09870c37
subramanayam12 <subramanyam933@gmail.com>
2023-09-30 13:43:39 +0530

    ading changes

--- ecomapp/admin.py
+++ ecomapp/admin.py
@@ -2,8 +2,3 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-@admin.register(Cart)
-class ProductAdmin(admin.ModelAdmin):
-    class Meta:
-        models=Cart
-        fields=['user','id','title','discountprice']
--- ecomapp/serializers.py
+++ ecomapp/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from .models import Cart
 from django.contrib.auth.models import User
 from rest_framework.authtoken.views import Token
 
